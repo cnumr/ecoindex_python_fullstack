1ee0c016da3ceb3b7bbc46f12356e13bcb242dd0
Vincent Vatelot <vincent.vatelot@ext.adeo.com>
2023-11-20 10:46:22 +0100

    doc(api): add comments to ecoindex service

--- bases/ecoindex/backend/services/ecoindex.py
+++ bases/ecoindex/backend/services/ecoindex.py
@@ -12,6 +12,22 @@ from requests import get
 async def get_latest_result_by_url(
     url: AnyHttpUrl, refresh: bool, version: Version
 ) -> EcoindexSearchResults:
+    """
+    Get the latest ecoindex result for a given url. This function will first try to find
+    an exact match for the url path, and if it doesn't find any, it will return the latest
+    result for the host.
+
+    Results are cached for 1 week by default. If you want to force the refresh of the cache,
+    set the refresh parameter to True.
+
+    params:
+        url: the url to search for
+        refresh: if True, will force the refresh of the cache
+        version: the version of the ecoindex to use
+
+    returns:
+        EcoindexSearchResults: the results for the given url
+    """
     ecoindex_cache = cache.set_cache_key(key=f"ecoindex-{url.host}/{url.path}")
     cached_results = await ecoindex_cache.get()
 
@@ -58,9 +74,24 @@ async def get_latest_result_by_url(
 async def get_badge(
     url: AnyHttpUrl, refresh: bool, version: Version, theme: str
 ) -> str:
+    """
+    Get the badge for a given url. This function will use the method `get_latest_result_by_url`.
+    If the url is not found, it will return the badge for the grade "unknown".
+
+    This returns the badge that [are hosted on jsdelivr.net](https://cdn.jsdelivr.net/gh/cnumr/ecoindex_badge@1/assets/svg/).
+
+    params:
+        url: the url to search for
+        refresh: if True, will force the refresh of the cache
+        version: the version of the ecoindex to use
+        theme: the theme of the badge to use (light or dark)
+
+    returns:
+        str: the badge image
+    """
     results = await get_latest_result_by_url(url=url, refresh=refresh, version=version)
 
-    grade = results.latest_result.grade if results.count > 0 else "unknown"
+    grade = results.latest_result.grade if results.latest_result else "unknown"
     ecoindex_cache = cache.set_cache_key(key=f"badge-{grade}-{theme}")
 
     cached_badge = await ecoindex_cache.get()
